95d90baa10554ec68c00c2b20c807a975bf828e9
marcell-ferencz-databricks <116736023+marcell-ferencz-databricks@users.noreply.github.com>
2023-01-20 17:13:50 +0000

    Update utils with loss function method

--- utils/mlflow_utils.py
+++ utils/mlflow_utils.py
@@ -2,6 +2,11 @@ import shutil
 import mlflow
 import os
 import json 
+import math
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as ss
 
 from splink.charts import save_offline_chart
 
@@ -103,3 +108,64 @@ def get_linker_charts(linker, log_parameters_charts, log_profiling_charts):
         charts_dict["blocking_rules_chart"] = blocking_rules_chart
 
     return charts_dict
+
+
+# ======================================================================
+# ======================================================================
+def get_match_probabilty_loss(predictions):
+    '''
+    Calculates the custom loss of a pariwise Splink prediction dataframe, by
+    1. Fitting 2 Gaussians on the probability distribution
+    2. Calculating and summing the means from 0 and 1 (respectively) and the standard deviations of the Gaussians
+    Parameters
+    ----------
+    predictions: pandas.DataFrame, df of predictions that must contain the match_probabilty column
+    Returns
+        A 2-tuple of the loss (float) and a chart (matplotlib.pyplot.figure) of the distribution and the fit
+    '''
+    
+    # check if match_probability column is in the dataframe
+    if "match_probability" in predictions.columns:
+        pred = predictions["match_probability"].values
+    else:
+        raise Exception("The match_probability column wasn't found in dataframe. You need to pass a dataframe that was predicted by Splink.")
+        
+    # Fig Gaussian mixture model (disable autologging for this)
+    mlflow.autolog(disable=True)
+    gm = GaussianMixture(n_components=2, random_state=0).fit(pred.reshape(-1, 1))
+    mlflow.autolog(disable=False)
+    
+    # Get means, variances and standard deviations
+    mean_1 = gm.means_[0][0]
+    mean_2 = gm.means_[1][0]
+    cov_1 = gm.covariances_[0][0][0]
+    cov_2 = gm.covariances_[1][0][0]
+    std_1 = math.sqrt(cov_1)
+    std_2 = math.sqrt(cov_2)
+
+    # Calculate loss
+    mean_goodness = mean_1 + 1.0 - mean_2
+    std_goodness = std_1 + std_2
+    prediction_purity = mean_goodness + std_goodness
+
+    # Create probability density functions based on the two sets of parameters
+    x = np.linspace(0.0, 1.0, 100)
+    gauss_1 = ss.norm.pdf(x, mean_1, std_1)
+    gauss_2 = ss.norm.pdf(x, mean_2, std_2)
+
+    # plot chart
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.hist(pred, density=True, bins=50)
+    ax.plot(x, gauss_1, c="green", linewidth=3)
+    ax.plot(x, gauss_2, c="orange", linewidth=3)
+    ax.axvline(mean_1, c="green", linewidth=1, linestyle="--")
+    ax.axvline(mean_2, c="orange", linewidth=1, linestyle="--")
+    ax.axvline(0.0, c="grey", linewidth=1, linestyle="--")
+    ax.axvline(1.0, c="grey", linewidth=1, linestyle="--")
+    ax.hlines(40, 0.0, mean_1, linewidth=2, linestyle="-", color="red")
+    ax.hlines(40, mean_2, 1.0, linewidth=2, linestyle="-", color="red")
+    ax.hlines(40, mean_1, mean_1+cov_1, linewidth=2, linestyle="-", color="yellow")
+    ax.hlines(45, mean_2-cov_2, mean_2, linewidth=2, linestyle="-", color="yellow")
+    ax.set_title("Probability density of predictions and fitted parameters")
+    
+    return prediction_purity, fig
