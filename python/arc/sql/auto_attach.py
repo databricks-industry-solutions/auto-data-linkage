f5d5a598d70f674615fe6363ae84d0313c4d21c6
milos.colic <milos.colic@databricks.com>
2023-03-18 23:52:22 +0000

    Fix broken test. Fix broken autoattach logic.

--- python/arc/sql/auto_attach.py
+++ python/arc/sql/auto_attach.py
@@ -6,40 +6,40 @@ from pyspark.sql import SparkSession
 
 
 def auto_attach():
-    spark = SparkSession.builder.getOrCreate()
-
-    _auto_attach_enabled = (
-            spark.conf.get("spark.databricks.industry.solutions.jar.autoattach", "false") == "true"
-    )
-
-    if _auto_attach_enabled:
-        sc = SparkContext.getOrCreate()
-        _jar_filename = f"arc-{importlib.metadata.version('databricks-arc')}-jar-with-dependencies.jar"
-        with importlib.resources.path("arc.lib", _jar_filename) as p:
-            _jar_path = p.as_posix()
-        JavaURI = getattr(sc._jvm.java.net, "URI")
-        JavaJarId = getattr(sc._jvm.com.databricks.libraries, "JavaJarId")
-        ManagedLibraryId = getattr(
-            sc._jvm.com.databricks.libraries, "ManagedLibraryId"
+    spark = SparkSession.getActiveSession()
+    if spark is not None:
+        _auto_attach_enabled = (
+                spark.conf.get("spark.databricks.industry.solutions.jar.autoattach", "false") == "true"
         )
-        ManagedLibraryVersions = getattr(
-            sc._jvm.com.databricks.libraries, "ManagedLibraryVersions"
-        )
-        NoVersion = getattr(ManagedLibraryVersions, "NoVersion$")
-        NoVersionModule = getattr(NoVersion, "MODULE$")
-        DatabricksILoop = getattr(
-            sc._jvm.com.databricks.backend.daemon.driver, "DatabricksILoop"
-        )
-        converters = sc._jvm.scala.collection.JavaConverters
 
-        JarURI = JavaURI.create("file:" + _jar_path)
-        lib = JavaJarId(
-            JarURI,
-            ManagedLibraryId.defaultOrganization(),
-            NoVersionModule.simpleString(),
-        )
-        libSeq = converters.asScalaBufferConverter((lib,)).asScala().toSeq()
+        if _auto_attach_enabled:
+            sc = SparkContext.getOrCreate()
+            _jar_filename = f"arc-{importlib.metadata.version('databricks-arc')}-jar-with-dependencies.jar"
+            with importlib.resources.path("arc.lib", _jar_filename) as p:
+                _jar_path = p.as_posix()
+            JavaURI = getattr(sc._jvm.java.net, "URI")
+            JavaJarId = getattr(sc._jvm.com.databricks.libraries, "JavaJarId")
+            ManagedLibraryId = getattr(
+                sc._jvm.com.databricks.libraries, "ManagedLibraryId"
+            )
+            ManagedLibraryVersions = getattr(
+                sc._jvm.com.databricks.libraries, "ManagedLibraryVersions"
+            )
+            NoVersion = getattr(ManagedLibraryVersions, "NoVersion$")
+            NoVersionModule = getattr(NoVersion, "MODULE$")
+            DatabricksILoop = getattr(
+                sc._jvm.com.databricks.backend.daemon.driver, "DatabricksILoop"
+            )
+            converters = sc._jvm.scala.collection.JavaConverters
 
-        context = DatabricksILoop.getSharedDriverContextIfExists().get()
-        context.registerNewLibraries(libSeq)
-        context.attachLibrariesToSpark(libSeq)
+            JarURI = JavaURI.create("file:" + _jar_path)
+            lib = JavaJarId(
+                JarURI,
+                ManagedLibraryId.defaultOrganization(),
+                NoVersionModule.simpleString(),
+            )
+            libSeq = converters.asScalaBufferConverter((lib,)).asScala().toSeq()
+
+            context = DatabricksILoop.getSharedDriverContextIfExists().get()
+            context.registerNewLibraries(libSeq)
+            context.attachLibrariesToSpark(libSeq)
--- python/test/utils/spark_test_case.py
+++ python/test/utils/spark_test_case.py
@@ -1,26 +1,29 @@
 import unittest
-import os
 from importlib.metadata import version
 
+import arc
 from pyspark.sql import SparkSession
 
-import arc
 
 class SparkTestCase(unittest.TestCase):
-
     spark = None
     library_location = None
 
     @classmethod
     def setUpClass(cls) -> None:
         SparkSession._instantiatedContext = None
+        SparkSession.builder._sc = None
         cls.library_location = f"{arc.__path__[0]}/lib/arc-{version('databricks-arc')}-jar-with-dependencies.jar"
         cls.spark = (
             SparkSession.builder.master("local")
-            .config("spark.jars", cls.library_location)
-            .getOrCreate()
+                .config("spark.jars", cls.library_location)
+                .getOrCreate()
         )
         cls.spark.conf.set("spark.databricks.industry.solutions.arc.jar.autoattach", "false")
+        log4jLogger = cls.spark.sparkContext._jvm.org.apache.log4j
+        LOGGER = log4jLogger.LogManager.getLogger(__name__)
+        cls.spark.sparkContext.setLogLevel("info")
+        LOGGER.info(SparkSession.builder.getOrCreate().sparkContext._jsc.sc().listJars())
         cls.spark.sparkContext.setLogLevel("warn")
 
     @classmethod
