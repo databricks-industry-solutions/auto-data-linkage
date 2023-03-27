e630e9c7033fde6cc88899d72c9d59e8fe750b0e
Marcell Ferencz <marcell.ferencz@databricks.com>
2023-03-27 17:57:44 +0100

    Manually setting prior to be 1/N

--- python/arc/autolinker/autolinker.py
+++ python/arc/autolinker/autolinker.py
@@ -614,8 +614,9 @@ class AutoLinker:
     linker = SparkLinker(data, spark=self.spark, database=self.schema, catalog=self.catalog)
     
     linker.initialise_settings(settings)
-    linker.estimate_probability_two_random_records_match(deterministic_rules, recall=0.8)
-    linker.estimate_u_using_random_sampling(target_rows=1e7)
+    # linker.estimate_probability_two_random_records_match(deterministic_rules, recall=0.8)
+    linker._settings_obj._probability_two_random_records_match = 1/self.data_rowcount
+    linker.estimate_u_using_random_sampling(target_rows=self.data_rowcount)
     for training_rule in training_rules:
       linker.estimate_parameters_using_expectation_maximisation(training_rule)
 
