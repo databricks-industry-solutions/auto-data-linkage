3575b90daf2bf7c3a4c6a4f601d989622f86ab6c
Marcell Ferencz <marcell.ferencz@databricks.com>
2023-06-19 10:09:42 +0100

    fixed first notebook

--- 01_Download_Dataset.py
+++ 01_Download_Dataset.py
@@ -1,9 +1,11 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC You may find this series of notebooks at https://github.com/databricks-industry-solutions/sample-repo. For more information about this solution accelerator, visit https://www.databricks.com/solutions/accelerators/sample-accelerator
+
+# COMMAND ----------
+
 import pandas as pd
 
-## NEED TO CHANGE BRANCH
 df = spark.createDataFrame(pd.read_csv("https://raw.githubusercontent.com/databricks-industry-solutions/auto-data-linkage/example_notebook_update/datasets/febrl1.csv"))
 
 df.write.mode("overwrite").saveAsTable("febrl1")
