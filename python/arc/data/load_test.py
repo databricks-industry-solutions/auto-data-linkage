567ec6039b5f6c97a566ee941971fdb8bed13c7e
robert.whiffin <robert.whiffin@databricks.com>
2023-07-18 08:50:38 +0100

    x

--- python/arc/data/load_test.py
+++ python/arc/data/load_test.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import importlib.resources
 
 def load_dummy_data():
-    df = pd.read_csv("/data/arc_febrl1.csv")
+    #files = importlib.resources.path("arc.data", "arc_febrl1.csv")
+    with importlib.resources.path("arc.data", "arc_febrl1.csv") as p:
+                        csv_path = p.as_posix()
+    df = pd.read_csv (csv_path)
     return df
