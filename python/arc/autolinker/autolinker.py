02083e6ad6c5382f489af83ef6d4dede2dadd900
robert.whiffin <robert.whiffin@databricks.com>
2023-09-21 16:18:11 +0100

    updating unique id default logic

--- python/arc/autolinker/autolinker.py
+++ python/arc/autolinker/autolinker.py
@@ -944,7 +944,7 @@ class AutoLinker:
     -------
 
     '''
-    columns = [x.columns for x in data]
+    columns = [filter(lambda x: x != self.unique_id, x.columns) for x in data]
 
     # write sql to lowercase and remove all non alpha-numeric characters
     cleaning_sql = 'lower(regexp_replace({column},  "[^0-9a-zA-Z]+", "")) as {column}'
