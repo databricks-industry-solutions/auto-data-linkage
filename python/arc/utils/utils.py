de5ef71ee8cbb6390c16a1a27c3054534bba328e
robert.whiffin <robert.whiffin@databricks.com>
2023-10-03 16:31:33 +0100

    move experiment clean up to outside autolinker

---
+++ python/arc/utils/utils.py
@@ -0,0 +1,24 @@
+from mlflow import MlflowClient
+
+def delete_all_default_linking_experiments():
+    '''
+    This method removes all linking experiments created by ARC with the default naming convention.
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    '''
+    if input("WARNING - this will delete all your ARC generated MLFlow experiments, Type 'YES' to proceed") != "YES":
+        return
+    spark = globals()["spark"]
+    username = spark.sql('select current_user() as user').collect()[0]['user']
+    pattern = f"%{username}/Databricks Autolinker%"
+
+    client = MlflowClient()
+    experiments = (
+        client.search_experiments(filter_string=f"name LIKE '{pattern}'")
+    )  # returns a list of mlflow.entities.Experiment
+    for exp in experiments:
+        client.delete_experiment(exp.experiment_id)
