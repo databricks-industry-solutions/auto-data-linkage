f3123f76a3b2078fe07bf500bfa008e814e7cbd0
robert.whiffin <robert.whiffin@databricks.com>
2023-10-10 07:57:46 +0100

    update logic to handle empty set

--- scala/src/main/scala/com/databricks/industry/solutions/arc/expressions/base/CountAccumulatorMap.scala
+++ scala/src/main/scala/com/databricks/industry/solutions/arc/expressions/base/CountAccumulatorMap.scala
@@ -11,7 +11,12 @@ case class CountAccumulatorMap(counter: Map[String, Long]) extends Serializable 
     def merge(other: CountAccumulatorMap): CountAccumulatorMap = {
         val newKeys = other.counter.keySet ++ counter.keySet
         val newMap_pre = newKeys.map { k => k -> (other.counter.getOrElse(k, 0L) + counter.getOrElse(k, 0L)) }.toMap
-        val newMap = newMap_pre.filter(_._2 > 1)
+        val newMap_pre_2 = newMap_pre.filter(_._2 > 1)
+        if (newMap_pre_2.isEmpty()) {
+            newMap = counter.take(Math.min(counter.size, 10))
+        } else {
+            newMap = newMap_pre2
+        }
         CountAccumulatorMap(newMap)
     }
 
