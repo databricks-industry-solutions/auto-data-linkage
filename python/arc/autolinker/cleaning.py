d7fbce8be12b4e6e5d9b2ef665a6e7037c9d5f38
robert.whiffin <robert.whiffin@databricks.com>
2023-10-27 12:38:12 +0100

    pull out cleanig code

---
+++ python/arc/autolinker/cleaning.py
@@ -0,0 +1,64 @@
+import pyspark
+import pyspark.sql.functions as F
+import typing
+
+def _clean_columns(
+        cleaning_mode: str,
+        data: pyspark.sql.DataFrame,
+        attribute_columns: list):
+    """
+    Method to clean string columns (turn them to lower case and remove non-alphanumeric characters)
+    in order to help with better (and quicker) string-distance calculations. If cleaning is 'all'
+    (as is by default), it will automatically clean as much as it can.  If cleaning is 'none', it will do nothing.
+    cleaning can also be a dictionary with keys as column names and values as lists of method strings.
+    The currently available cleaning methods are turning to lower case and removing non-alphanumeric characters.
+
+    Returns a Spark DataFrame.
+
+    :param data: DataFrame containing the data to be cleaned
+    :param attribute_columns: all attribute columns
+    :param cleaning: string or dicitonary with keys as column names and values as list of valid cleaning method names.
+
+    """
+
+    cleaning = cleaning_mode
+    # if cleaning is set to "none", don't do anything to the data
+    if cleaning == "none":
+        return data
+
+    # if it's set to "all", turn it into a dictionary covering all possible cases
+    if cleaning == "all":
+        cleaning = {col: ["lower", "alphanumeric_only"] for col in attribute_columns}
+
+    for col, methods in cleaning.items():
+        # if column is not a string, skip it
+        if not data.schema[col].dataType == StringType():
+            continue
+
+        for method in methods:
+            if method == "alphanumeric_only":
+                # replace column and only keep alphanumeric and space characters
+                data = data.withColumn(col, F.regexp_replace(F.col(col), r"[^A-Za-z0-9 ]+", ""))
+
+            elif method == "lower":
+                data = data.withColumn(col, F.lower(F.col(col)))
+
+    return data
+
+
+def _do_column_cleaning(
+        cleaning_mode
+        , autolink_data
+        , linker_mode
+        , attribute_columns
+) -> typing.Union[pyspark.sql.dataframe.DataFrame, list]:
+    # Clean the data
+    if linker_mode == "dedupe_only":
+        output_data = _clean_columns(cleaning_mode, autolink_data, attribute_columns)
+    else:
+        output_data = [
+            _clean_columns(cleaning_mode, autolink_data[0], attribute_columns),
+            _clean_columns(cleaning_mode, autolink_data[1], attribute_columns)
+        ]
+    return output_data
+
---
+++ python/test/test_cleaning.py
@@ -0,0 +1,1 @@
+# Todo - use pyspark testing to evaluate
